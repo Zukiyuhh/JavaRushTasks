67a4ec18373fb38dbda0f3373b4cf6152749c63f
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-18 22:19:17 +0300

    add solution from JavaSyntax from task 0102

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0102/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task01.task0102;
+
+/* 
+Включить аварийное питание и ручное управление
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Аварийное питание включено \n" +
+                            "Ручное управление включено");
+
+    }
+}
