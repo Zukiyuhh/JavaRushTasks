8fd75f38815bcea9b959ee9569855ef81df38acf
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-20 19:55:17 +0300

    add JavaSyntax solution from task 0104

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0104/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task01.task0104;
+
+/* 
+Починить систему уборки города
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Уборка города началась");//dasd
+        System.out.println("убираем мусор");
+        System.out.println("убираем листья");
+        System.out.println("уборка города закончилась");
+    }
+}
