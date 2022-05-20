ebb0651d0bb0b56fcdff803690a632497b5e7fab
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-21 00:38:25 +0300

    add JavaSyntax solution from task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 123;
+        int numericValue = 321;
+        int size = 312;
+        int number = 222;
+
+
+    }
+}
