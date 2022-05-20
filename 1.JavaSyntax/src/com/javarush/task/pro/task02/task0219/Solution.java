64f96bfd520356bf3017d30e50bc9526d76c9ca8
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-21 00:33:23 +0300

    add JavaSyntax solution from task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int age = 23;
+        int id = 44;
+        String documents = "ok";
+        String behavior = "calm";
+
+
+    }
+}
