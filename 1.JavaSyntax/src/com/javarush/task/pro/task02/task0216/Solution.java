c298c40ae0ded588203d7d9771df6ce1a66e9dfb
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-26 01:02:57 +0300

    add JavaSyntax solution from task 0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Zubr";
+        String date = "in may";
+        String time = "19";
+        String adjective = "aiemed on";
+        String noun = "hard";
+        String verb = "reaching";
+
+        System.out.println("Новости:");
+
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
