173b9f4dd75e225c4da95402d6c55f01f7947f5a
kirillzubrin <Kirill.zubrin@mail.ru>
2022-05-27 22:24:44 +0300

    add JavaSyntax solution from task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "How";
+        String phrase = "How are yoy managing?";
+        String line = "IM";
+        String text = "adafsadasd";
+
+    }
+}
