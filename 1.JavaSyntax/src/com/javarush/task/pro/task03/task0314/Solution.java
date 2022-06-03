024d912fb31fad67b2964de003dcaba5e42defa3
kirillzubrin <Kirill.zubrin@mail.ru>
2022-06-03 13:00:43 +0300

    JavaRush tasks 0405

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -12,10 +12,27 @@ public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String string = console.nextLine();
+        int number;
+        double aDouble = console.nextDouble();
+        if (console.hasNextInt()){
+            number = console.nextInt();
+        }
+
+
+
+
+
+
+
+
+        /*
+        String string = console.nextLine();
+
         if (secret.equalsIgnoreCase(string)) {
             System.out.println("доступ разрешен");
         } else {
             System.out.println("доступ запрещен");
         }
+         */
     }
 }
